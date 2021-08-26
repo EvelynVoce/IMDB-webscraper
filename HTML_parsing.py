3319224ee12d572dcaa8b7d3204457ee9015bc21
JamieVoce <jamievoce0115@gmail.com>
2021-08-26 22:30:03 +0100

    Scrapes one IMBD page to check if page has more than 250 user reviews

---
+++ HTML_parsing.py
@@ -0,0 +1,34 @@
+import requests
+
+
+class HtmlParsing:
+
+    @staticmethod
+    def request_html(my_url):
+        continue_collecting_data = True
+        page_html = None
+        # try:
+        page_html = requests.get(my_url, stream=True).text
+
+        # except:
+            # continue_collecting_data = False
+            # print("CONNECTION ERROR")
+        return continue_collecting_data, page_html
+
+    @staticmethod
+    def get_amount_of_reviews(page_soup) -> bool:
+        amount_of_user_reviews_span = page_soup.find("span", {"class": "score"}).text
+        met_requirements = False
+
+        if amount_of_user_reviews_span[-1] == "K":
+            thousands_of_reviews = amount_of_user_reviews_span.strip("K")
+            amount_of_user_reviews = int(float(thousands_of_reviews) * 1000)
+        else:
+            amount_of_user_reviews = int(amount_of_user_reviews_span)
+
+        print("User reviews", amount_of_user_reviews)
+
+        if amount_of_user_reviews > 250:
+            met_requirements = True
+
+        return met_requirements
---
+++ Webscraper.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup as Soup
+import gc  # garbage collector - really useful for RAM management
+import HTML_parsing
+
+
+def setup():
+    with open("film_completed.txt", "r") as films_done:
+        list_of_links_completed = films_done.read().splitlines()
+    with open("film_incompleted.txt", "r") as films_not_done:
+        list_of_links_to_be_completed = films_not_done.read().splitlines()
+    return list_of_links_to_be_completed[0], list_of_links_to_be_completed, list_of_links_completed
+
+
+def get_data(parse, page_soup):
+    met_requirements = parse.get_amount_of_reviews(page_soup)
+
+
+def main():
+    my_url, list_of_links_to_be_completed, list_of_links_completed = setup()
+    
+    while len(list_of_links_to_be_completed) > 0:
+        parse = HTML_parsing.HtmlParsing()
+        continue_collecting_data, page_html = parse.request_html(my_url)
+
+        if continue_collecting_data:
+            page_soup = Soup(page_html, "lxml")
+            get_data(parse, page_soup)
+
+        else:  # The line below is neccessary (I want it to get a connection error and loop back and error again so I know it's not an internet issue)
+            print("Connection error: ", my_url)
+
+
+main()
