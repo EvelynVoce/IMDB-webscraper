365f38e7e70a9e97b9730807b0bf025db6bb69f9
JamieVoce <jamievoce0115@gmail.com>
2021-10-08 23:07:17 +0100

    HasMetRequirements test case added

--- WebscraperTest.py
+++ WebscraperTest.py
@@ -3,19 +3,31 @@ from HTML_parsing import HtmlParsing
 from requests import Session
 
 session = Session()
+
 endgame_link = "https://www.imdb.com/title/tt4154796/"
-parse = HtmlParsing(endgame_link, session)
+parsed_endgame = HtmlParsing(endgame_link, session)
+
+russian_link = "https://www.imdb.com/title/tt0386134/?ref_=adv_li_tt"
+parsed_russian_film = HtmlParsing(russian_link, session)
 
 
 class SetToString(unittest.TestCase):
     def test_empty_set(self):
-        self.assertEqual("", parse.set_to_string({}))
+        self.assertEqual("", parsed_endgame.set_to_string({}))
 
     def test_singular_element(self):
-        self.assertEqual("Ab1_$", parse.set_to_string(["Ab1_$"]))
+        self.assertEqual("Ab1_$", parsed_endgame.set_to_string(["Ab1_$"]))
 
     def test_changing_comma(self):  # Tests that commas are being replaced with semi-colons
-        self.assertEqual("Ab1_$; B*", parse.set_to_string(["Ab1_$", 'B*']))
+        self.assertEqual("Ab1_$; B*", parsed_endgame.set_to_string(["Ab1_$", 'B*']))
+
+
+class HasMetRequirements(unittest.TestCase):
+    def test_popular_film(self):
+        self.assertEqual(True, parsed_endgame.met_requirements)
+
+    def test_unpopular_film(self):
+        self.assertEqual(False, parsed_russian_film.met_requirements)
 
 
 def suite():
