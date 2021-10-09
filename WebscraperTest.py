def07ec6597586b4af84075b4f1cf78a52360978
JamieVoce <jamievoce0115@gmail.com>
2021-10-09 09:43:13 +0100

    SetTitle tests added

--- WebscraperTest.py
+++ WebscraperTest.py
@@ -7,8 +7,8 @@ session = Session()
 endgame_link = "https://www.imdb.com/title/tt4154796/"
 parsed_endgame = HtmlParsing(endgame_link, session)
 
-russian_link = "https://www.imdb.com/title/tt0386134/?ref_=adv_li_tt"
-parsed_russian_film = HtmlParsing(russian_link, session)
+slender_man_link = "https://www.imdb.com/title/tt5690360/"
+parsed_slender_man_film = HtmlParsing(slender_man_link, session)
 
 
 class SetToString(unittest.TestCase):
@@ -27,17 +27,18 @@ class HasMetRequirements(unittest.TestCase):
         self.assertEqual(True, parsed_endgame.met_requirements)
 
     def test_unpopular_film(self):
+        russian_link = "https://www.imdb.com/title/tt0386134/"
+        parsed_russian_film = HtmlParsing(russian_link, session)
         self.assertEqual(False, parsed_russian_film.met_requirements)
 
 
-def suite():
-    test_suite = unittest.TestSuite
-    test_suite.addTest(unittest.makeSuite(SetToString))
-    return test_suite
+class SetTitle(unittest.TestCase):
+    def test_title_endgame(self):
+        self.assertEqual('Avengers: Endgame', parsed_endgame.title)
+
+    def test_title_slender_man(self):
+        self.assertEqual("Slender Man", parsed_slender_man_film.title)
 
 
 if __name__ == '__main__':
-    suite = suite()
-    unit_test_runner = unittest.TextTestRunner()
-    unit_test_runner.run(suite)
-
+    unittest.main()
