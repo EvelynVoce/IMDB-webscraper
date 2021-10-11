3df49cea774d506d82b155c175932a4b244236c2
JamieVoce <jamievoce0115@gmail.com>
2021-10-11 20:36:24 +0100

    Fixed an issue with configuration files not running the script when pulled from the repository

--- WebscraperTest.py
+++ WebscraperTest.py
@@ -70,6 +70,43 @@ def test_cast(expected_answer, object_passed):
     assert expected_answer == actual_answer
 
 
+@pytest.mark.parametrize("expected_answer, object_passed",
+                         [({'The Dark Knight', 'Avengers Assemble', 'Black Panther', 'Avengers: Age of Ultron',
+                            'Thor: Ragnarok', 'Iron Man', 'Avengers: Infinity War',
+                            'Captain America: The Winter Soldier', 'Guardians of the Galaxy', 'Joker',
+                            'Captain America: Civil War', 'Doctor Strange'}, parsed_endgame),
+
+                          ({'Annabelle Comes Home', 'The Possession of Hannah Grace', 'Slender Man: Origins',
+                            'Pet Sematary', 'The Bye Bye Man', 'The Curse of La Llorona', 'Annabelle',
+                            'Insidious: The Last Key', 'Slender', 'Ouija',
+                            'The Nun', 'Truth or Dare'}, parsed_slender_man_film)])
+def test_related_films(expected_answer, object_passed):
+    actual_answer = object_passed.get_related_films()
+    assert expected_answer == actual_answer
+
+
+@pytest.mark.parametrize("expected_answer, object_passed",
+                         [(['https://www.imdb.com/title/tt4154756/', 'https://www.imdb.com/title/tt0848228/',
+                            'https://www.imdb.com/title/tt2395427/', 'https://www.imdb.com/title/tt3501632/',
+                            'https://www.imdb.com/title/tt7286456/', 'https://www.imdb.com/title/tt3498820/',
+                            'https://www.imdb.com/title/tt0468569/', 'https://www.imdb.com/title/tt1211837/',
+                            'https://www.imdb.com/title/tt1825683/', 'https://www.imdb.com/title/tt1843866/',
+                            'https://www.imdb.com/title/tt2015381/', 'https://www.imdb.com/title/tt0371746/'],
+                           parsed_endgame),
+
+                          (['https://www.imdb.com/title/tt4030600/', 'https://www.imdb.com/title/tt1204977/',
+                           'https://www.imdb.com/title/tt5814060/', 'https://www.imdb.com/title/tt4913966/',
+                           'https://www.imdb.com/title/tt8350360/', 'https://www.imdb.com/title/tt5734576/',
+                           'https://www.imdb.com/title/tt3322940/', 'https://www.imdb.com/title/tt6772950/',
+                           'https://www.imdb.com/title/tt10551346/', 'https://www.imdb.com/title/tt0837563/',
+                           'https://www.imdb.com/title/tt3317158/', 'https://www.imdb.com/title/tt5726086/'],
+                          parsed_slender_man_film)])
+def test_related_films(expected_answer, object_passed):
+    actual_answer = object_passed.get_related_urls()
+    print(actual_answer)
+    assert expected_answer == actual_answer
+
+
 # import unittest
 # from HTML_parsing import HtmlParsing
 # from requests import Session
