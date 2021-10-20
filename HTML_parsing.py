a581779ab8fe89321351edb84650a258cfcc2847
JamieVoce <jamievoce0115@gmail.com>
2021-10-20 22:32:11 +0100

    Added HTML_parsing type hints

--- HTML_parsing.py
+++ HTML_parsing.py
@@ -8,34 +8,34 @@ class HtmlParsing:
     def __init__(self, my_url, session):
         page_html = session.get(my_url, stream=True).text
         self.page_soup = Soup(page_html, "lxml")
-        self.met_requirements = self.has_met_requirements()
+        self.met_requirements: bool = self.has_met_requirements()
         if self.met_requirements:
-            self.title = self.set_title()
-            self.date = self.set_date()
-            self.rating = self.get_rating()
-            self.genres = self.set_to_string(self.get_genre())
-            self.directors = ""
-            self.writers = ""
+            self.title: str = self.set_title()
+            self.date: str = self.set_date()
+            self.rating: str = self.get_rating()
+            self.genres: str = self.set_to_string(self.get_genre())
+            self.directors: str = ""
+            self.writers: str = ""
             self.get_writers_and_directors()
-            self.cast = self.set_to_string(self.get_cast())
-            self.related_films = self.set_to_string(self.get_related_films())
-            self.links_to_related_films = self.get_related_urls()
+            self.cast: str = self.set_to_string(self.get_cast())
+            self.related_films: str = self.set_to_string(self.get_related_films())
+            self.links_to_related_films: list = self.get_related_urls()
 
     @staticmethod
     def set_to_string(input_list):
-        converted_string = str(input_list)
+        converted_string: str = str(input_list)
         for ch in [("[", ""), ("]", ""), ("{", ""), ("}", ""), ('"', ""), ("'", ""), (",", ";")]:
-            converted_string = converted_string.replace(ch[0], ch[1])
+            converted_string: str = converted_string.replace(ch[0], ch[1])
         return converted_string
 
     def has_met_requirements(self):
-        amount_of_user_reviews_span = self.page_soup.find("span", {"class": "score"}).text
+        amount_of_user_reviews_span: str = self.page_soup.find("span", {"class": "score"}).text
         if amount_of_user_reviews_span[-1] == "K":
-            amount_of_user_reviews = float(amount_of_user_reviews_span[:-1]) * 1000
+            amount_of_user_reviews: float = float(amount_of_user_reviews_span[:-1]) * 1000
         else:
-            amount_of_user_reviews = int(amount_of_user_reviews_span)
+            amount_of_user_reviews: int = int(amount_of_user_reviews_span)
 
-        minimum_amount_user_reviews = 125
+        minimum_amount_user_reviews: int = 125
         return amount_of_user_reviews > minimum_amount_user_reviews
 
     def set_title(self):
@@ -44,7 +44,7 @@ class HtmlParsing:
 
     def set_date(self):
         date_div = self.page_soup.find("div", {"class": "TitleBlock__TitleMetaDataContainer-sc-1nlhx7j-2 hWHMKr"})
-        found_date = date_div.find("a").text.strip()
+        found_date: str = date_div.find("a").text.strip()
         return found_date.split("–")[0]
 
     def get_rating(self):
@@ -53,20 +53,19 @@ class HtmlParsing:
     def get_genre(self):
         genre_div = self.page_soup.find("div", {"class": "ipc-chip-list GenresAndPlot__GenresChipList-cum89p-4 gtBDBL"})
         genres_a_tags = genre_div.findAll("a")
-        genres_set = {genre.text for genre in genres_a_tags}
+        genres_set: set = {genre.text for genre in genres_a_tags}
 
         tv_tag = self.page_soup.find("li", text="TV Series")
         tv_mini_tag = self.page_soup.find("li", text="TV Mini Series")
         if tv_tag or tv_mini_tag:
             genres_set.add("TV Series")
-
         return genres_set
 
     def get_writers_and_directors(self):
         credit_divs = self.page_soup.findAll("div", {"class": "ipc-metadata-list-item__content-container"})
         for index, div in enumerate(credit_divs[:2]):
             credit_div_a = div.findAll("a")
-            temporary_set = {name.text for name in credit_div_a}
+            temporary_set: set = {name.text for name in credit_div_a}
             if index == 0:
                 self.directors = self.set_to_string(temporary_set)
             else:
@@ -81,7 +80,7 @@ class HtmlParsing:
         return {film.text for film in liked_films_all_data}
 
     def get_related_urls(self):
-        root_link = "https://www.imdb.com"
+        root_link: str = "https://www.imdb.com"
         related_films_div = self.page_soup.findAll("div", {"class": "ipc-poster ipc-poster--base ipc-poster--dynamic"
                                                                     "-width ipc-poster-card__poster ipc-sub-grid-item"
                                                                     " ipc-sub-grid-item--span-2"})
