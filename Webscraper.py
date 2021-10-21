b1ed6c51b2e6e6ec3ce27773217fe699e5007a46
EvelynVoce <jamievoce0115@gmail.com>
2021-10-21 23:00:11 +0100

    Removed the need to read the links completed in the while loop. Also removed duplicated code which read the links to scrape file

--- Webscraper.py
+++ Webscraper.py
@@ -15,13 +15,10 @@ def fetch(link, session):
 
 def main():
     links_scraped_file: str = "films_completed.txt"
-    links_to_be_completed: list = file_handling.retrieve_file(file_handling.LINKS_TO_SCRAPE_FILE)
     while links_to_be_completed:
-        list_of_links_completed = file_handling.retrieve_file(links_scraped_file)
-        set_of_links_to_be_completed: set = {link for link in links_to_be_completed
-                                             if link not in list_of_links_completed}
+        links_to_be_completed: list = file_handling.retrieve_file(file_handling.LINKS_TO_SCRAPE_FILE)
+        set_of_links_to_be_completed: set = {link for link in links_to_be_completed}
         t1 = perf_counter()
-
         with ThreadPoolExecutor(max_workers=30) as p, Session() as session:
             p.map(fetch, set_of_links_to_be_completed, [session] * len(set_of_links_to_be_completed))
         print("Iteration complete", perf_counter() - t1)
@@ -31,7 +28,6 @@ def main():
         list_of_film_data.clear()
         file_handling.update_text_file(links_scraped_file, set_of_links_to_be_completed, "a")
         file_handling.update_text_file(file_handling.LINKS_TO_SCRAPE_FILE, links_found, "w")
-        links_to_be_completed = file_handling.retrieve_file(file_handling.LINKS_TO_SCRAPE_FILE)
 
 
 if __name__ == "__main__":
